c67e7b0fe721ebd7cb1fcd491cb731256da67042
Robert Goik <robert.goik@gmail.com>
2022-02-16 21:10:35 +0100

    02 Hello from CDN

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MY REACT APP
 
 01. Let's start
+02. Hello from CDN
---
+++ index.js
@@ -0,0 +1,5 @@
+const App = React.createElement("h1", null, "Hello World");
+
+const root = document.getElementById("root");
+
+ReactDOM.render(App, root);
